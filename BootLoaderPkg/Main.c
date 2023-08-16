941ac4084baab53badcfa599a4018757f0ebda8f
tommy <tommydsha@gmail.com>
2023-08-16 20:34:46 +0900

    メモリマップの取得処理を追加

--- BootLoaderPkg/Main.c
+++ BootLoaderPkg/Main.c
@@ -1,12 +1,50 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
+
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID* buffer;
+  UINTN map_size;
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
+
+
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  if (map->buffer == NULL) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  map->map_size = map->buffer_size;
+  return gBS->GetMemoryMap(
+    &map->map_size,
+    (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+    &map->map_key,
+    &map->descriptor_size,
+    &map->descriptor_version
+  );
+}
+
 
 EFI_STATUS EFIAPI UefiMain(
   EFI_HANDLE image_handle,
   EFI_SYSTEM_TABLE *system_table
 ) {
   Print(L"Hello, TommyOS World!\n");
-  
+
+  CHAR8 memmap_buf[4096 * 4];
+  struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0}; 
+  GetMemoryMap(&memmap);
+
+  Print(L"All done\n");
+
   while (1); // NOTE: return で関数から抜けるとqemuが終了する（？）
   return EFI_SUCCESS;
 }
