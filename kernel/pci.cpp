dec2d43f72c4042caf87bb95961a63cded851d78
tommy <tommydsha@gmail.com>
2023-08-19 22:26:25 +0900

    PCIコンフィグレーション空間の読み書きを実装

--- kernel/pci.cpp
+++ kernel/pci.cpp
@@ -1,5 +1,5 @@
 #include "pci.hpp"
-
+#include "asmfunc.hpp"
 
 uint32_t MakeAddress(uint8_t bus, uint8_t device, uint8_t function, uint8_t reg_offset) {
   auto shl = [](uint32_t x, unsigned int bits) {
@@ -11,4 +11,47 @@ uint32_t MakeAddress(uint8_t bus, uint8_t device, uint8_t function, uint8_t reg_
     | shl(device, 11)
     | shl(function, 8)
     | (reg_offset & 0xfc); // NOTE: オフセットは4バイト単位（4の倍数）→下位2ビットが0になるようにマスクする
+}
+
+
+void WriteAddress(uint32_t address) {
+  IoOut32(kConfigAddress, address);
+}
+
+void WriteData(uint32_t data) {
+  IoOut32(kConfigData, data);
+}
+
+uint32_t ReadData() {
+  IoIn32(kConfigData);
+}
+
+uint16_t ReadVendorId(uint8_t bus, uint8_t device, uint8_t function) {
+  auto address = MakeAddress(bus, device, function, 0x00);
+  WriteAddress(address);
+  return ReadData() & 0xffffu;
+}
+
+uint16_t ReadDeviceId(uint8_t bus, uint8_t device, uint8_t function) {
+  auto address = MakeAddress(bus, device, function, 0x00);
+  WriteAddress(address);
+  return ReadData() >> 16;
+}
+
+uint8_t ReadHeaderType(uint8_t bus, uint8_t device, uint8_t function) {
+  auto address = MakeAddress(bus, device, function, 0x0c);
+  WriteAddress(address);
+  return (ReadData() >> 16) & 0xffu;
+}
+
+uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function) {
+  auto address = MakeAddress(bus, device, function, 0x08);
+  WriteAddress(address);
+  return ReadData();
+}
+
+uint32_t ReadBusNumbers(uint8_t bus, uint8_t device, uint8_t function) {
+  auto address = MakeAddress(bus, device, function, 0x18);
+  WriteAddress(address);
+  return ReadData();
 }
--- kernel/pci.hpp
+++ kernel/pci.hpp
@@ -5,4 +5,14 @@
 const uint16_t kConfigAddress = 0x0cf8;
 const uint16_t kConfigData    = 0x0cfc;
 
-uint32_t MakeAddress(uint8_t bus, uint8_t device, uint8_t function, uint8_t reg_offset); 
+uint32_t MakeAddress(uint8_t bus, uint8_t device, uint8_t function, uint8_t reg_offset);
+
+void WriteAddress(uint32_t address);
+void WriteData(uint32_t data);
+uint32_t ReadData();
+
+uint16_t ReadVendorId(uint8_t bus, uint8_t device, uint8_t function);
+uint16_t ReadDeviceId(uint8_t bus, uint8_t device, uint8_t function);
+uint8_t ReadHeaderType(uint8_t bus, uint8_t device, uint8_t function);
+uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function);
+uint32_t ReadBusNumbers(uint8_t bus, uint8_t device, uint8_t function);
