d6f6681fd0fd4c86daece55416d0a117dc441d90
tommy <tommydsha@gmail.com>
2023-08-19 15:46:27 +0900

    sprintfを使えるように実装

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -4,3 +4,4 @@
 
 void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color);
 const uint8_t* GetFont(char c);
+void WriteString(PixelWriter& writer, int x, int y, const char* s, const PixelColor& color);
