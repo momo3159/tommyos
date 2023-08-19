dadd9874a5b646e17efbe68a0b31cb9ae09692d3
tommy <tommydsha@gmail.com>
2023-08-19 16:50:38 +0900

    コンソールを実装

---
+++ kernel/console.cpp
@@ -0,0 +1,51 @@
+#include <cstring>
+#include "console.hpp"
+#include "font.hpp"
+
+
+Console::Console(
+  PixelWriter& writer_, 
+  const PixelColor& fg_color_, 
+  const PixelColor& bg_color_) 
+: writer{writer_}, fg_color{fg_color_}, bg_color{bg_color_},
+  buffer{}, cursor_row{0}, cursor_column{0} {}
+
+void Console::PutString(const char* s) {
+  while(*s) {
+    if (*s == '\n') {
+      NewLine(); ++s;
+      continue;
+    } 
+    
+
+    if (cursor_column >= kColumns) {
+      // NOTE: kColumnsより引数の文字列が長い場合、このif文がないとバグになる？
+      NewLine();
+    }
+
+    WriteAscii(writer, 8 * cursor_column, 16 * cursor_row, *s, fg_color);
+    buffer[cursor_row][cursor_column] = *s;
+    ++cursor_column; 
+    ++s;
+  }
+}
+
+void Console::NewLine() {
+  cursor_column = 0;
+  if (cursor_row < kRows - 1) {
+    // NOTE: == kRows の場合、次の行に空きがないので不可
+    ++cursor_row;
+  } else {
+    for (int y=0;y<16*kRows;++y) {
+      for (int x=0;x<8*kColumns;++x) {
+        writer.Write(x, y, bg_color);
+      }
+    }
+
+    for (int row=0;row<kRows-1;++row) {
+      memcpy(buffer[row], buffer[row+1], kColumns+1);
+      WriteString(writer, 0, 16*row, buffer[row], fg_color);
+    }
+    memset(buffer[kRows-1], 0, kColumns+1);
+  }
+}
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -4,6 +4,7 @@
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "font.hpp"
+#include "console.hpp"
 
 
 int WritePixel(const FrameBufferConfig& config, int x, int y, const PixelColor& c) {
@@ -48,22 +49,12 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     }
   }
 
-  for (int x=0;x<200;x++) {
-    for (int y=0;y<200;y++) {
-      pixel_writer->Write(100 + x, 100 + y, {0, 255, 0});
-    }
-  }
-
-  int i=0;
-  for (char c='!';c<='~';c++, i++) {
-    WriteAscii(*pixel_writer, 8*i, 50, c, {0, 0, 0});
-  }
-
-  WriteString(*pixel_writer, 0, 66, "Hello World!", {0, 0, 255});
-
   char buf[128];
-  sprintf(buf, "1 + 2 = %d", 1 + 2);
-  WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
-  
+  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  for (int i=0;i<27;i++) {
+    sprintf(buf, "line %d\n", i);
+    console.PutString(buf);
+  }
+
   while(1) __asm__("hlt");
 }
