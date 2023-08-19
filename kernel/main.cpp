a6971b51627987edd7e079daa09906631216dcaf
tommy <tommydsha@gmail.com>
2023-08-19 18:29:19 +0900

    マウスカーソルを定義
    マウスカーソルを描画

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -27,6 +27,35 @@ void* operator new(size_t size, void* buf) {
 }
 void operator delete(void* obj) noexcept {}
 
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+  "@              ",
+  "@@             ",
+  "@.@            ",
+  "@..@           ",
+  "@...@          ",
+  "@....@         ",
+  "@.....@        ",
+  "@......@       ",
+  "@.......@      ",
+  "@........@     ",
+  "@.........@    ",
+  "@..........@   ",
+  "@...........@  ",
+  "@............@ ",
+  "@......@@@@@@@@",
+  "@......@       ",
+  "@....@@.@      ",
+  "@...@ @.@      ",
+  "@..@   @.@     ",
+  "@.@    @.@     ",
+  "@@      @.@    ",
+  "@       @.@    ",
+  "         @.@   ",
+  "         @@@   ",
+};
+
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
@@ -58,23 +87,16 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       break;
   }
 
-
-  for (int x=0;x<frame_buffer_config.horizontal_resolution;x++) {
-    for (int y=0;y<frame_buffer_config.vertical_resolution;y++) {
-      pixel_writer->Write(x, y, {255, 255, 255});
-    }
-  }
-
   console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-  char buf[128];
-  for (int i=0;i<27;i++) {
-    sprintf(buf, "line %d\n", i);
-    console->PutString(buf);
-  }
-
-  for (int i=0;i<27;i++) {
-    printk("printk: %d\n", i);
+  for (int dy=0;dy<kMouseCursorHeight;dy++) {
+    for (int dx=0;dx<kMouseCursorWidth;dx++) {
+      if (mouse_cursor_shape[dy][dx] == '@') {
+        pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+      } else if (mouse_cursor_shape[dy][dx] == '.') {
+        pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+      }
+    }
   }
 
   while(1) __asm__("hlt");
