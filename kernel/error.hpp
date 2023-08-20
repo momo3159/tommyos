58d0ca090225e46f7df367dddaf6caa664e77ce1
tommy <tommydsha@gmail.com>
2023-08-20 15:32:08 +0900

    エラーを表すenumを定義

---
+++ kernel/error.hpp
@@ -0,0 +1,31 @@
+#pragma once 
+#include <array>
+
+class Error {
+  public: 
+    enum Code {
+      kSuccess,
+      kFull,
+      kEmpty,
+      kLastOfCode,
+    };
+
+    Error(Code code_) : code{code_} {}
+
+    operator bool() const {
+      return this->code != kSuccess;
+    }
+
+    const char* Name() const {
+      return code_names[static_cast<int>(this->code)];
+    }
+
+  private:
+    static constexpr std::array<const char*, 3> code_names = {
+      "kSuccess",
+      "kFull",
+      "kEmpty",
+    };
+
+    Code code;
+};
