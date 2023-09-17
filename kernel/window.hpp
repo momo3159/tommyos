717aab02821fe955da85814059b3392c32e6710d
tommy <tommydsha@gmail.com>
2023-09-17 16:35:54 +0900

    描画領域を表すWindowクラスを実装

---
+++ kernel/window.hpp
@@ -0,0 +1,42 @@
+#pragma once 
+#include <vector>
+#include <optional>
+#include "graphics.hpp"
+
+class Window {
+  public: 
+    class WindowWriter : public PixelWriter {
+      // Window::data_に色を設定する
+      // 実際にフレームバッファに書き込むのはWindow::DrawTo
+      public: 
+        WindowWriter(Window& window) : window_{window} {}
+        virtual void Write(int x, int y, const PixelColor& c) override {
+          window_.At(x, y) = c;
+        }
+        virtual int Width() const override { return window_.Width(); }
+        virtual int Height() const override { return window_.Height(); }
+
+      private:
+        Window& window_;
+    };
+
+    Window(int width, int height);
+    ~Window() = default;
+    Window(const Window& rhs) = delete;
+    Window& operator=(const Window& rhs) = delete;
+    
+    void DrawTo(PixelWriter& writer, Vector2D<int> position);
+
+    int Height() const;
+    int Width() const;
+    PixelColor& At(int x, int y);
+    const PixelColor& At(int x, int y) const;
+    WindowWriter* Writer();
+    void SetTransparentColor(std::optional<PixelColor> c);
+
+  private:
+    int width_, height_;
+    std::vector<std::vector<PixelColor>> data_{};
+    WindowWriter writer_{*this};
+    std::optional<PixelColor> transparent_color_{std::nullopt};
+};
