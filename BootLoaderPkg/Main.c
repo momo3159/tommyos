850a56db9f84eb16cb4e2c9020fde11ddd9a94d0
tommy <tommydsha@gmail.com>
2024-05-03 17:30:35 +0900

    ブートローダからカーネルファイルを読み込む

--- BootLoaderPkg/Main.c
+++ BootLoaderPkg/Main.c
@@ -6,6 +6,7 @@
 #include <Protocol/SimpleFileSystem.h>
 #include <Protocol/DiskIo2.h>
 #include <Protocol/BlockIo.h>
+#include <Guid/FileInfo.h>
 
 struct MemoryMap {
   UINTN buffer_size; // 格納用バッファの大きさ
@@ -134,6 +135,22 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_tab
   );
   SaveMemoryMap(&memmap, memmap_file);
   memmap_file->Close(memmap_file);
+
+  // カーネルファイルの情報取得
+  EFI_FILE_PROTOCOL* kernel_file;
+  root_dir->Open(root_dir, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ, 0);
+
+  UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12; // \kernel.elf 分だけ余分に容量を確保
+  UINT8 file_info_buffer[file_info_size];
+  kernel_file->GetInfo(kernel_file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
+
+  EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer; // バイト列として読み込んだものをキャスト
+  UINTN kernel_file_size = file_info->FileSize;
+
+  EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+  gBS->AllocatePages(AllocateAddress, EfiLoaderData, (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+  kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+  Print(L"kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
 
   Print(L"All done\n");
 
