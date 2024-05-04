0e01b54cfae05b5609bd04fd0ac49b96f2acd419
tommy <tommydsha@gmail.com>
2024-05-04 21:18:00 +0900

    PixelFormatごとにクラスを実装

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,5 +1,15 @@
 #include <cstdint>
+#include <cstddef>
 #include "frame_buffer/frame_buffer_config.hpp"
+
+// new 演算子の挙動
+// new の第一引数はコンパイラが勝手に埋めてくれる
+// メモリ確保したら実際にインスタンス生成をする
+void* operator new(size_t sizse, void* buf) {
+  return buf;
+}
+
+void operator delete(void* obj) noexcept {}
 
 struct PixelColor {
   uint8_t r, g, b;
@@ -59,16 +69,28 @@ int WritePixel(const FrameBufferConfig& config, int x, int y, const PixelColor& 
   return 0;
 }
 
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter* pixel_writer;
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+  switch (frame_buffer_config.pixel_format) {
+    case kPixelBGRResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf) BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+      break;
+    case kPixelRGBResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf) RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+      break;
+  }
+
   for (int x=0;x<frame_buffer_config.horizontal_resolution;x++) {
     for (int y=0;y<frame_buffer_config.vertical_resolution;y++) {
-      WritePixel(frame_buffer_config, x, y, {255, 255, 255});
+      pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
 
   for (int x=0;x<200;x++) {
     for (int y=0;y<100;y++) {
-      WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+      pixel_writer->Write(100 + x, 100 + y, {0, 255, 0});
     }
   }
   while (1) __asm__("hlt");
