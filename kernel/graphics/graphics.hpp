b878dfbf4285cbfc5ece2c522eae9986c0c67989
tommy <tommydsha@gmail.com>
2024-06-06 23:32:31 +0900

    windowの定義

--- kernel/graphics/graphics.hpp
+++ kernel/graphics/graphics.hpp
@@ -10,6 +10,14 @@ struct PixelColor {
   uint8_t r, g, b;
 };
 
+inline bool operator==(const PixelColor& lhs, const PixelColor& rhs) {
+  return lhs.r == rhs.r && lhs.g == rhs.g && lhs.b == rhs.b;
+}
+
+inline bool operator!=(const PixelColor& lhs, const PixelColor& rhs) {
+  return !(lhs == rhs);
+}
+
 static const PixelColor BLACK = PixelColor{0, 0, 0};
 static const PixelColor WHITE = PixelColor{255, 255, 255};
 static const PixelColor BLUE  = PixelColor{45, 118, 237};
@@ -29,28 +37,38 @@ struct Vector2D {
 
 class PixelWriter {
   public:
-    PixelWriter(const FrameBufferConfig& config) : config_{config} {} // コンストラクタ
     virtual ~PixelWriter() = default; // デストラクタ
     virtual void Write(int x, int y, const PixelColor& c) = 0; // 0 は純粋仮想関数を表す == 実装を持たない。
+    virtual int Width() const = 0;
+    virtual int Height() const = 0;
+};
 
+
+class FrameBufferWriter : public PixelWriter {
+  public:
+    FrameBufferWriter(const FrameBufferConfig& config) : config_{config} {}
+    virtual ~FrameBufferWriter() = default;
+    virtual int Width() const override { return config_.horizontal_resolution; }
+    virtual int Height() const override { return config_.vertical_resolution; }
+    
   protected:
     uint8_t* PixelAt(int x, int y) {
       return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
     }
-  
+
   private:
     const FrameBufferConfig& config_;
 };
 
-class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+class BGRResv8BitPerColorPixelWriter : public FrameBufferWriter {
   public:
-    using PixelWriter::PixelWriter; // コンストラクタは親のものを使う
+    using FrameBufferWriter::FrameBufferWriter; // コンストラクタは親のものを使う
     virtual void Write(int x, int y, const PixelColor& c) override;
 };
 
-class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+class RGBResv8BitPerColorPixelWriter : public FrameBufferWriter {
   public:
-    using PixelWriter::PixelWriter; // コンストラクタは親のものを使う
+    using FrameBufferWriter::FrameBufferWriter; // コンストラクタは親のものを使う
     virtual void Write(int x, int y, const PixelColor& c) override;
 };
 
---
+++ kernel/window/window.hpp
@@ -0,0 +1,48 @@
+#pragma once 
+
+#include <vector>
+#include <optional>
+#include "../graphics/graphics.hpp"
+
+class Window {
+  public:
+    // Window に紐づくPixelWriter
+    // Window の data を更新する役割
+    class WindowWriter : public PixelWriter {
+      public:
+        WindowWriter(Window& window) : window_{window} {}
+        virtual void Write(int x, int y, const PixelColor& c) {
+          window_.At(x, y) = c;
+        }
+        virtual int Width() const override { return window_.Width(); }
+        virtual int Height() const override { return window_.Width(); }
+      private:
+        Window& window_;
+    };
+
+    Window(int width, int height);
+    ~Window() = default;
+    Window(const Window& rhs) = delete;
+    Window& operator=(const Window& rhs) = delete;
+
+    /**
+     * PixelWriter でウィンドウの表示領域を描画
+     * @param writer 描画先
+     * @param position writer の左上を基準とした描画位置
+    */
+    void DrawTo(PixelWriter& writer, Vector2D<int> position);
+
+    PixelColor& At(int x, int y);
+    const PixelColor& At(int x, int y) const;
+    int Width() const;
+    int Height() const;
+    WindowWriter* Writer();
+    
+    void SetTransparentColor(std::optional<PixelColor> c);
+  private:
+    int width_, height_;
+    std::vector<std::vector<PixelColor>> data_{}; 
+    WindowWriter writer_{*this};
+    std::optional<PixelColor> transparent_color_{std::nullopt};
+};
+
